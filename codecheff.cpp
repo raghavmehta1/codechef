b3e3a492804c59f7c919b3aea9142c0b7d6cb25b
raghavmehta1 <71930941+raghavmehta1@users.noreply.github.com>
2022-03-17 21:03:27 +0530

    Add files via upload

---
+++ codecheff.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int t;
+cin>>t;
+while(t--){
+    int c,x,y;
+    cin>>c>>x>>y;
+    int chocneed=c-x;
+    cout<<chocneed*y<<endl;
+}
+return 0;
+}
